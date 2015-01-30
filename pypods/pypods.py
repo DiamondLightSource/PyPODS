379b179da78b7c9eb1bbc60849c2427f53579020
Tom Cobb <thomas.cobb@gmail.com>
2015-01-30 15:33:10 +0000

    Add simple PODS server.  Still doesn't talk to PVManager.

--- pypods/pypods.py
+++ pypods/pypods.py
@@ -4,7 +4,8 @@ The top-level PyPODS websocket server.
 from ws4py.websocket import WebSocket
 import json
 
-from pvmanager import PVManager
+#from pvmanager import PVManager
+import mock
 
 from wsgiref.simple_server import make_server
 from ws4py.websocket import EchoWebSocket
@@ -13,20 +14,44 @@ from ws4py.server.wsgiutils import WebSocketWSGIApplication
 
 
 
-pvm = PVManager()
+class PodsServer(object):
+
+    def __init__(self):
+        self._pvm = mock.MagicMock()
+        self._channels = {}
+
+    def dispatch(self, j):
+        msg_type = j["message"]
+        try:
+            if msg_type == "subscribe":
+                self.add_sub(j)
+            elif msg_type == "event":
+                self.event(j)
+
+        except Exception as e:
+            print("Something went wrong!")
+            print(e)
+
+    def add_sub(self, j):
+        pv = self._pvm.read_and_write(j["channel"])
+        self._channels[j["id"]] = pv
+        print("channels: %s", self._channels)
+
+    def event(self, j):
+        print("received event %s"  % j)
+
 
 class PodsWebSocket(WebSocket):
+
+    def __init__(self, a, b, c, d):
+        WebSocket.__init__(self, a, b, c, d)
+        self.ps = PodsServer()
 
     def received_message(self, message):
         print("received: %s" % message)
         print("type: %s" % type(message))
         j = json.loads(str(message))
-        try:
-            if j["message"] == "subscribe":
-                pvm.read_and_write(j["channel-name"])
-        except Exception as e:
-            print("Something went wrong!")
-            print(e)
+        self.ps.dispatch(j)
 
     def opened(self):
         print("Websocket opened.")
