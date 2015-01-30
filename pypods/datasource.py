747a82ef366f12f1535ee9551024bed82a76c363
Matt Clarke <matt.clarke@stfc.ac.uk>
2015-01-30 15:52:19 +0000

    Added datasource base class - currently does not match

---
+++ pypods/datasource.py
@@ -0,0 +1,8 @@
+
+class DataSource(object):
+    def __init__(self):
+        pass
+
+    def create_channel(self, channel_name):
+        """Creates a channel handler for the given name"""
+        pass
