b2907c5972f592e83517c4cd20c8948c61f82a73
Matt Clarke <matt.clarke@stfc.ac.uk>
2015-01-30 15:56:18 +0000

    Added channelhandler base

---
+++ pypods/channelhandler.py
@@ -0,0 +1,22 @@
+
+class ChannelHandler(object):
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def write(self, value, callback):
+        pass
+
+    def add_write_callback(self, callback):
+        pass
+
+    def add_read_callback(self, callback):
+        pass
+
+    def remove(self):
+        pass
